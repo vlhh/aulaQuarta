00072063e5247d2fee7c70b0f0cae49f3d0b4e9d
vlhh <89313640+vlhh@users.noreply.github.com>
2022-08-24 22:08:45 -0300

    melhorias

--- JavaApplication2/src/ProjetoNota1/TelaContas.java
+++ JavaApplication2/src/ProjetoNota1/TelaContas.java
@@ -14,9 +14,28 @@ public class TelaContas extends javax.swing.JFrame {
     /**
      * Creates new form TelaContas
      */
+    
+    Conta objetoConta;
+    
     public TelaContas() {
         initComponents();
-        lblTitulo.setText("Titulo");
+        
+        Conta objetoConta = new Conta("Viória",13123123,2000,213123);        
+        System.out.println("saldo na conta: " +objetoConta.getSaldoConta());
+        
+        Conta objetoConta2 = new Conta("Viória2",82347234,800,234234);        
+        System.out.println("saldo na conta: "+ objetoConta2.getSaldoConta());
+        
+        
+        //prencheer os campos na tela
+        lblNomeConta1.setText("nome: " + objetoConta.getTitular());
+        lblNomeConta2.setText("nome: " + objetoConta2.getTitular());
+        
+        lblNumero.setText("num : " +objetoConta.getNumConta());
+        lblNumero2.setText("num : " +objetoConta2.getNumConta());
+        
+        lblSaldo.setText("saldo :" +objetoConta.getSaldoConta());
+        lblSaldo2.setText("saldo :" +objetoConta2.getSaldoConta());
         
     }
 
@@ -30,7 +49,12 @@ public class TelaContas extends javax.swing.JFrame {
     private void initComponents() {
 
         bntAlterarNome = new javax.swing.JButton();
-        lblTitulo = new javax.swing.JLabel();
+        lblNomeConta1 = new javax.swing.JLabel();
+        lblNomeConta2 = new javax.swing.JLabel();
+        lblNumero = new javax.swing.JLabel();
+        lblNumero2 = new javax.swing.JLabel();
+        lblSaldo = new javax.swing.JLabel();
+        lblSaldo2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -44,9 +68,20 @@ public class TelaContas extends javax.swing.JFrame {
             }
         });
 
-        lblTitulo.setFont(new java.awt.Font("Trebuchet MS", 1, 15)); // NOI18N
-        lblTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblTitulo.setText("alterar titulo");
+        lblNomeConta1.setFont(new java.awt.Font("Trebuchet MS", 1, 15)); // NOI18N
+        lblNomeConta1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblNomeConta1.setText("conta1");
+
+        lblNomeConta2.setFont(new java.awt.Font("Trebuchet MS", 1, 15)); // NOI18N
+        lblNomeConta2.setText("conta2");
+
+        lblNumero.setText("número conta");
+
+        lblNumero2.setText("número conta");
+
+        lblSaldo.setText("saldo");
+
+        lblSaldo2.setText("saldo");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -55,28 +90,50 @@ public class TelaContas extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(130, 130, 130)
+                        .addGap(118, 118, 118)
                         .addComponent(bntAlterarNome, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(21, 21, 21)
-                        .addComponent(lblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(24, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblNomeConta1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(lblSaldo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(lblNumero, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE))))
+                        .addGap(69, 69, 69)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblNomeConta2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(lblSaldo2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(lblNumero2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)))))
+                .addContainerGap(50, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(54, 54, 54)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblNomeConta1, javax.swing.GroupLayout.DEFAULT_SIZE, 54, Short.MAX_VALUE)
+                    .addComponent(lblNomeConta2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblNumero, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblNumero2, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblSaldo, javax.swing.GroupLayout.DEFAULT_SIZE, 27, Short.MAX_VALUE)
+                    .addComponent(lblSaldo2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 109, Short.MAX_VALUE)
                 .addComponent(bntAlterarNome, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(133, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void bntAlterarNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bntAlterarNomeActionPerformed
-        lblTitulo.setText("novo titulo");
+        lblNomeConta1.setText("saldo");
     }//GEN-LAST:event_bntAlterarNomeActionPerformed
 
     /**
@@ -116,6 +173,11 @@ public class TelaContas extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bntAlterarNome;
-    private javax.swing.JLabel lblTitulo;
+    private javax.swing.JLabel lblNomeConta1;
+    private javax.swing.JLabel lblNomeConta2;
+    private javax.swing.JLabel lblNumero;
+    private javax.swing.JLabel lblNumero2;
+    private javax.swing.JLabel lblSaldo;
+    private javax.swing.JLabel lblSaldo2;
     // End of variables declaration//GEN-END:variables
 }
